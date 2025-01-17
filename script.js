2ab051166d2bca887b0d442007d1a38fcb4a7441
Rasheed <107322169+rasheedballim@users.noreply.github.com>
2025-01-17 17:35:24 +0200

    Added reset button

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 // create a ref to the div in html
 const container = document.querySelector('.container');
 
+
+
+
+
 // loop 256 times to make a grid
 for (let i = 0; i < 256; i++) {
     // creates a new div and referencing it to 'content'
@@ -14,7 +18,7 @@ const squares = document.querySelectorAll(".content");
 squares.forEach((button => {
     button.addEventListener("mouseover", () =>{
         console.log('Mouse entered');
-        button.style.backgroundColor = 'lightblue';
+        button.style.backgroundColor = getRandomColour();
 
     })
 }))
@@ -24,7 +28,23 @@ function getRandomColour(){
     let randomColour = "#";
     for (let i =0; i<6;i++){
         randomVal = Math.floor((Math.random()*16));
+        randomColour = randomColour + letters[randomVal];
+        
 
     }
-
+    return randomColour;
 }
+
+
+
+
+
+function resetColours(){
+    squares.forEach(div => {
+    div.style.backgroundColor = 'white';
+});
+    
+
+} 
+const btn = document.querySelector("#reset_button");
+btn.addEventListener("click", resetColours);
