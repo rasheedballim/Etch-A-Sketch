6f1e2c85150e7244888289c5cbef556f4021c268
Rasheed <107322169+rasheedballim@users.noreply.github.com>
2025-01-20 10:50:48 +0200

    Added comments

--- script.js
+++ script.js
@@ -1,84 +1,66 @@
-// create a ref to the div in html
-const container = document.querySelector('.container');
-
-
-
-
 function gridCreate(number) {
-    container.innerHTML = ''; 
-    for (let i = 0; i < (number*number); i++) {
-    const content = document.createElement('div');
-    content.classList.add('content');
+  // create a ref to the div in html
+  const container = document.querySelector(".container");
+  // clears content in the container 
+  container.innerHTML = "";
+  // creating each block as per users request
+  for (let i = 0; i < number * number; i++) {
+    const content = document.createElement("div");
+    content.classList.add("content");
     container.appendChild(content);
-}
-const squares = document.querySelectorAll(".content");
+  }
 
-let isMouseDown = false;
-document.addEventListener("mousedown", (event) =>{
-    
-    if(event.button ===0){
-        isMouseDown = true;
+  let isMouseDown = false;
+  document.addEventListener("mousedown", (event) => {
+    if (event.button === 0) {
+      isMouseDown = true;
     }
-});
+  });
 
-document.addEventListener("mouseup",() =>{
+  document.addEventListener("mouseup", () => {
     isMouseDown = false;
-});
+  });
+  const squares = document.querySelectorAll(".content");
 
-squares.forEach((button) => {
+  squares.forEach((button) => {
     button.addEventListener("mouseover", () => {
-        if (isMouseDown) { 
-            button.style.backgroundColor = getRandomColour();
-        }
+      if (isMouseDown) {
+        button.style.backgroundColor = getRandomColour();
+      }
     });
-});
+  });
 
+  function resetColours() {
+    squares.forEach((div) => {
+      div.style.backgroundColor = "white";
+    });
+  }
+  const btn = document.querySelector("#reset_button");
+  btn.addEventListener("click", resetColours);
+  let contents = document.querySelectorAll(".content");
 
-function resetColours(){
-    squares.forEach(div => {
-    div.style.backgroundColor = 'white';
-});
-    
-
-} 
-const btn = document.querySelector("#reset_button");
-btn.addEventListener("click", resetColours);
-let contents = document.querySelectorAll('.content');
-
-contents.forEach(content => {
-  content.style.setProperty('flex', `0 0 calc(650px / ${number})`);
-  content.style.setProperty('height', `calc(650px / ${number})`);
-});
-
-
-
-
-
+  contents.forEach((content) => {
+    content.style.setProperty("flex", `0 0 calc(650px / ${number})`);
+    content.style.setProperty("height", `calc(650px / ${number})`);
+  });
 }
 
-
-function getRandomColour(){
-    const letters = '0123456789ABCDEF';
-    let randomColour = "#";
-    for (let i =0; i<6;i++){
-        randomVal = Math.floor((Math.random()*16));
-        randomColour = randomColour + letters[randomVal];
-        
-
-    }
-    return randomColour;
+function getRandomColour() {
+  const letters = "0123456789ABCDEF";
+  let randomColour = "#";
+  for (let i = 0; i < 6; i++) {
+    randomVal = Math.floor(Math.random() * 16);
+    randomColour = randomColour + letters[randomVal];
+  }
+  return randomColour;
 }
 
+const inputField = document.querySelector("#number-input");
+const readInputField = document.querySelector("#confirm-button");
 
-
-
-
-
-
-const inputField = document.querySelector("#number-input")
-const readInputField = document.querySelector("#confirm-button")
-
-readInputField.addEventListener("click",()=>{
-    const inputValue = inputField.value;
-    gridCreate(inputValue);
-})
+// Event Listener that takes the value entered by user and
+// passes it through to the gridCreate fucntion
+readInputField.addEventListener("click", () => {
+  const inputValue = inputField.value;
+  gridCreate(inputValue);
+});
